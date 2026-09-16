a76f24a024cc723b5a4ce4d2512aac3022aca0c6
Stathis Gennatas <gennatas@gmail.com>
2026-09-16 11:16:43 -0700

    use SVG icon throughout

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 VERSION=$(python3 -c "import json; print(json.load(open('manifest.json'))['version'])")
 OUT="rtemis-${VERSION}.xpi"
 rm -f "$OUT"
-zip -r -X "$OUT" manifest.json background.js icons/icon-48.png icons/icon-96.png icons/favicon.svg newtab popup \
+zip -r -X "$OUT" manifest.json background.js icons/favicon.svg newtab popup \
   -x '*.DS_Store' '__MACOSX/*' '*/.*'
 echo "Built $OUT"
 unzip -l "$OUT"
